81eecfe8b87182e68d32f1af28414dd283a359c1
abdulazizsardar <abdulazizsardar58@gmail.com>
2024-08-20 18:26:46 +0600

    active deactive theme file path and error fix

--- inc/activitions-methods/cw-active-methods.php
+++ inc/activitions-methods/cw-active-methods.php
@@ -1,4 +1,3 @@
-
 <?php
 
 defined( 'ABSPATH' ) || die();
@@ -12,55 +11,28 @@ defined( 'ABSPATH' ) || die();
 //==================== Theme Active Check ==========================
 add_action('switch_theme', 'handle_theme_change');
 
-
-if ( ! function_exists('handle_theme_change')){
-    function handle_theme_change() {
+ function handle_theme_change() {
 	 
-        if (!function_exists('is_plugin_active')) {
-            include_once(ABSPATH . 'wp-admin/includes/plugin.php');
-        }
-        $theme_elementor_plugin = 'elementor/elementor.php'; 
-        $theme_cw_hfa_plugin = 'elementor_addon_header_footer/curlware-header-footer-elementor.php';
-        $theme_current_theme = wp_get_theme();
-       
-        if (!is_plugin_active($theme_elementor_plugin) || $theme_current_theme->get('Name') !== 'curlware') {
-            if (is_plugin_active($theme_cw_hfa_plugin)) {
-                deactivate_plugins($theme_cw_hfa_plugin);
-            }
-            remove_plugin_menu_items();
-        }
-    }
-}
- 
+	 if (!function_exists('is_plugin_active')) {
+		 include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+	 }
+	 $theme_elementor_plugin = 'elementor/elementor.php'; 
+	 $theme_cw_hfa_plugin = 'elementor_addon_header_footer/curlware-header-footer-elementor.php';
+	 $theme_current_theme = wp_get_theme();
+	
+	 if (!is_plugin_active($theme_elementor_plugin) || $theme_current_theme->get('Name') !== 'curlware') {
+		 if (is_plugin_active($theme_cw_hfa_plugin)) {
+			 deactivate_plugins($theme_cw_hfa_plugin);
+		 }
+		 remove_plugin_menu_items();
+	 }
+ }
  
 //==================== Plugin Active Check ==========================
 add_action('plugins_loaded', 'cw_check_and_handle_theme_for_plugin');
 
-if (! function_exists('cw_check_and_handle_theme_for_plugin')){
-    function cw_check_and_handle_theme_for_plugin() {
+function cw_check_and_handle_theme_for_plugin() {
  
-<<<<<<< HEAD
-        if ( ! function_exists('is_plugin_active')) {
-            include_once(ABSPATH . 'wp-admin/includes/plugin.php');
-        }
-    
-        $elementor_plugin = 'elementor/elementor.php'; 
-        $cw_hfa_plugin = 'elementor_addon_header_footer/curlware-header-footer-elementor.php';
-        $current_theme = wp_get_theme();
-    
-        if (is_plugin_active($elementor_plugin)) {
-            
-            if ($current_theme->get('Name') === 'curlware') {
-               
-                if (!is_plugin_active($cw_hfa_plugin)) {
-                    activate_plugin($cw_hfa_plugin);
-                }
-            } else {
-                if (is_plugin_active($cw_hfa_plugin)) {
-                    deactivate_plugins($cw_hfa_plugin);
-                }
-                add_action('admin_notices', 'notify_admin_to_switch_theme');
-=======
     if (!function_exists('is_plugin_active')) {
         include_once(ABSPATH . 'wp-admin/includes/plugin.php');
     }
@@ -71,59 +43,55 @@ if (! function_exists('cw_check_and_handle_theme_for_plugin')){
 
     if (is_plugin_active($elementor_plugin)) {
         
-        if ($current_theme->get('Name') === 'Tanvir Dev') {
+        if ($current_theme->get('Name') === 'curlware') {
            
             if (!is_plugin_active($cw_hfa_plugin)) {
                 activate_plugin($cw_hfa_plugin);
->>>>>>> 008168b2d26d0817667c5d3b352288835af29de9
             }
         } else {
-          
             if (is_plugin_active($cw_hfa_plugin)) {
                 deactivate_plugins($cw_hfa_plugin);
             }
-            add_action('admin_menu', 'remove_plugin_menu_items', 999);
-            add_action('admin_notices', 'notify_admin_to_activate_elementor');
+            add_action('admin_notices', 'notify_admin_to_switch_theme');
         }
+    } else {
+      
+        if (is_plugin_active($cw_hfa_plugin)) {
+            deactivate_plugins($cw_hfa_plugin);
+        }
+        add_action('admin_menu', 'remove_plugin_menu_items', 999);
+        add_action('admin_notices', 'notify_admin_to_activate_elementor');
     }
 }
-
-
 
 if(!function_exists('notify_admin_to_switch_theme')){
     function notify_admin_to_switch_theme() {
         ?>
         <div class="notice notice-warning is-dismissible">
             <p><?php echo esc_html('The "Curlware Header Footer Elementor Addon" plugin is only active when the "Curlware" theme is active. Please switch back to the "Curlware" theme to use this plugin.', 'curlware-header-footer-elementor'); ?></p>
-        </div> 
-        <?php
-    }
-}
+            <a href="<?php echo esc_url(admin_url('themes.php')); ?>" class="button-primary">
+                <?php echo esc_html('Switch to Curlware Theme'); ?>
+            </a>
 
-if( ! function_exists('notify_admin_to_activate_elementor') ){
-    function notify_admin_to_activate_elementor() {
-        ?>
-        <div class="notice notice-error is-dismissible"> 
-            <p><?php echo esc_html('The "Curlware Header Footer Elementor Addon" plugin requires the Elementor plugin to be active. Please activate Elementor to use this plugin.', 'curlware-header-footer-elementor'); ?></p> 
         </div> 
         <?php
     }
 }
 
 
-if( ! function_exists('remove_plugin_menu_items') ){
-    function remove_plugin_menu_items() {
+
+
+function notify_admin_to_activate_elementor() {
+    ?>
+    <div class="notice notice-error is-dismissible"> 
+        <p><?php echo esc_html('The "Curlware Header Footer Elementor Addon" plugin requires the Elementor plugin to be active. Please activate Elementor to use this plugin.', 'curlware-header-footer-elementor'); ?></p> 
+    </div> 
+    <?php
+}
+
+function remove_plugin_menu_items() {
 	
-<<<<<<< HEAD
-        if (function_exists('remove_menu_page')) {
-            remove_menu_page('elementor-hf'); 
-        }
-    }
-    
-}
-=======
 	if (function_exists('remove_menu_page')) {
 		remove_menu_page('elementor-hf'); 
 	}
 }
->>>>>>> 008168b2d26d0817667c5d3b352288835af29de9
